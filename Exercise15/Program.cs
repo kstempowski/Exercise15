e35b6f2b597174425265959775dc841cd09ac2a9
Your Name <kennystemper@gmail.com>
2021-04-10 15:35:47 -0400

    Exercise 15

---
+++ Exercise15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 15");
+            bool repeat = true;
+            while (repeat)
+            {
+                //Prompt the user for input
+                Console.WriteLine("Please enter a number greater than 0");
+                string input = Console.ReadLine();
+                int number = int.Parse(input);
+
+                //The cube of i will be produced until i equals the number input by the user
+                for (int i = 1; number >= i; i++)
+                {
+                    int cube = i * i * i;
+                    Console.WriteLine(cube);
+                }
+                //prompt the user for additional input
+                Console.WriteLine("Enter another number? y/n");
+                string answer = Console.ReadLine();
+
+                if (answer == "y")
+                {
+                    //An answer if "y" will repeat the program
+                    repeat = true;
+                }
+                else
+                {
+                    //Any answer other than "y" will terminate the program
+                    Console.WriteLine("Goodbye!");
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
